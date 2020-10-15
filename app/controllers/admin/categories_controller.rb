4f1388427c3930d8f922731e651cc2107f250fa2
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-15 08:52:39 -0700

    fixed warning for empty category insertion

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,21 +1,31 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   # for final project 
   # def index
-  #   render json: Category.all, only: [:id, :name ]
+  #   render json: Category.all, only: [:id, :name ]s
   # end
 
   def index
     @categories = Category.all 
-    @products =  []
-    @categories.each do |category|
-      @products.push(category.products)
-    end
   end
 
   def new
     @category = Category.new
   end
   
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
