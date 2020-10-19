4e16dcb249be1d17a3d86f13b54efb3a4918c672
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-19 09:19:10 -0700

    product-specs test setup

---
+++ spec/product_spec.rb
@@ -0,0 +1,8 @@
+Rspec.describe Product, type: :model do
+  describe "Validations" do
+    # validates :name, presence: true
+    # validates :price, presence: true
+    # validates :quantity, presence: true
+    # validates :category, presence: true
+  end
+end
